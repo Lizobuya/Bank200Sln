00d59a2c4ef12321265c4b48575751b7fb6a6dac
Lizobuya Danisa <ldanisa2@gmail.com>
2022-08-04 15:20:08 +0200

    ..

--- Bank200/Models/BankAccount.cs
+++ Bank200/Models/BankAccount.cs
@@ -2,7 +2,10 @@
 {
     public class BankAccount
     {
-        public int BankAccountTypeId { get; set; }
-      
+        public int BankAccountId { get; set; }
+        public string CustomerNum { get; set; }
+
+        public decimal AccountBalance { get; set; }
+
     }
 }
